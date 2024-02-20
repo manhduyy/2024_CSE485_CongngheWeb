602842dff76cc3b6d63673075e363b6ca85441e7
Ngô Mạnh Duy <119400510+manhduyy@users.noreply.github.com>
2024-02-20 17:20:43 +0700

    Main

---
+++ project12/index.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Navbar</title>
+    <!-- Font Awesome CSS -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+    <style>
+        /* CSS Reset */
+        * {
+            margin: 0;
+            padding: 0;
+            box-sizing: border-box;
+        }
+
+        /* Navbar Styles */
+        .navbar {
+            background-color: #0066FF;
+            overflow: hidden;
+        }
+
+        .navbar ul {
+            list-style-type: none;
+            margin: 0;
+            padding: 0;
+            overflow: hidden;
+        }
+
+        .navbar li {
+            float: left;
+        }
+
+        .navbar a {
+            display: block;
+            color: white;
+            text-align: center;
+            padding: 14px 20px;
+            text-decoration: none;
+        }
+
+        .navbar a:hover {
+            background-color: #FFFFFF;
+            color: black;
+        }
+
+        /* Icon Styles */
+        .navbar .fa {
+            margin-right: 5px;
+        }
+
+        @media screen and (max-width: 600px) {
+            .navbar a {
+                float: none;
+                display: block;
+                text-align: left;
+            }
+        }
+    </style>
+</head>
+<body>
+
+<?php
+$navItems =  [
+    "GIỚI THIỆU",
+    "TIN TỨC & THÔNG BÁO",
+    "TUYỂN SINH",
+    "ĐÀO TẠO",
+    "NGHIÊN CỨU",
+    "ĐỐI NGOẠI",
+    "VĂN BẢN",
+    "SINH VIÊN",
+    "LIÊN HỆ"
+];
+
+echo '<nav class="navbar"><ul>';
+// Chèn icon home cho mục "Home"
+echo '<li><a href="#"><i class="fa fa-home"></i>GIỚI THIỆU</a></li>';
+foreach ($navItems as $item) {
+    echo "<li><a href='#'>$item</a></li>";
+}
+echo '</ul></nav>';
+?>
+
+</body>
+</html>
